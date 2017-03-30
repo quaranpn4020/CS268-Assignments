a57a830df25db6e6b414e53247aad58b7ae25a98
Peter Quaranta <quaranpn4020@uwec.edu>
2017-03-30 15:09:29 -0500

    Assignment7 updates

--- Assignment7/orderConfirmation.php
+++ Assignment7/orderConfirmation.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="keywords" content="assignment 3, pizza, pepperoni, slices, pie, javascript, js">
+    <meta name="keywords" content="assignment 3, pizza, pepperoni, slices, pie, javascript, js, order confirmation">
     <meta name="description" content="A web page about pizza, made for assignment #5 of CS268">
-    <title>Pizza | Order</title>
+    <title>Pizza | Order Confirmation</title>
     <link rel="stylesheet" type="text/css" href="pizza.css">
     <script src="http://use.edgefonts.net/cabin-sketch.js"></script>
     <script src="http://use.edgefonts.net/passion-one.js"></script>
@@ -12,6 +12,12 @@
 </head>
 
 <body>
+    
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] != "POST") {
+            header("Location: http://yoda.cs.uwec.edu/CS268/students/quaranpn4020/Assignment7/order.html");    
+        }
+    ?>
     
     <div id="container">
         
@@ -24,54 +30,86 @@
                 <h1>
                     create your own 'za
                 </h1>
-                
-                <form name="pizzaOrderForm" onsubmit="return validateForm();" method="post" action="http://yoda.cs.uwec.edu/CS268/students/HARDTR/processForm.php">
-                    <table class="formTable">
-                        <tr>
-                            <td class="formLabel">
-                                Size:
-                            </td>
-                            <td>
-                                <select name="size">
-                                    <option value="">Choose a size</option>
-                                    <option value="s">Small</option>
-                                    <option value="m">Medium</option>
-                                    <option value="l">Large</option>
-                                    <option value="r">Ryan Sized</option>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="formLabel">
-                                Toppings:
-                            </td>
-                            <td>
-                                <input type="checkbox" name="pepperoni" value="pep">Pepperoni<br>
-                                <input type="checkbox" name="sausage" value="sau">Sausage<br>
-                                <input type="checkbox" name="onion" value="oni">Onion<br>
-                                <input type="checkbox" name="jalapeno" value="jal">Jalapeno<br>
-                                <input type="checkbox" name="greenpepper" value="grp">Green Pepper<br>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="formLabel">
-                                Comments:
-                            </td>
-                            <td>
-                                <textarea name="comments" rows="3" cols="40"></textarea>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="submit" name ="submit" value="Place Order">
-                            </td>
-                            <td>
-                                <input type="reset" value="Reset">
-                            </td>
-                        </tr>
-                    </table>
-                
-                </form>
+                <table class="formTable">
+                    <tr>
+                        <td class="formLabel">
+                            Size:
+                        </td>
+                        <td>
+                            <?php
+                                $sizeVal = $_POST["size"];
+                                if ($sizeVal == "s") {
+                                    echo "Small";
+                                }
+                                if ($sizeVal == "m") {
+                                    echo "Medium";
+                                }
+                                if ($sizeVal == "l") {
+                                    echo "Large";
+                                }
+                                if ($sizeVal == "r") {
+                                    echo "Ryan Sized";
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="formLabel">
+                            Toppings:
+                        </td>
+                        <td>
+                            <!--NumChecked is used to generate a linebreak between toppings-->
+                            <?php
+                                $numChecked = 0;
+                                if ($_POST["pepperoni"] == "pep") {
+                                    echo "Pepperoni";
+                                    $numChecked++;
+                                }
+                                if ($_POST["sausage"] == "sau") {
+                                    if ($numChecked > 0) {
+                                        echo "<br>";
+                                    }
+                                    echo "Sausage";
+                                    $numChecked++;
+                                }
+                                if ($_POST["onion"] == "oni") {
+                                    if ($numChecked > 0) {
+                                        echo "<br>";
+                                    }
+                                    echo "Onion";
+                                    $numChecked++;
+                                }
+                                if ($_POST["jalapeno"] == "jal") {
+                                    if ($numChecked > 0) {
+                                        echo "<br>";
+                                    }
+                                    echo "Jalapeno";
+                                    $numChecked++;
+                                }
+                                if ($_POST["greenpepper"] == "grp") {
+                                    if ($numChecked > 0) {
+                                        echo "<br>";
+                                    }
+                                    echo "Green Pepper";
+                                    $numChecked++;
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="formLabel">
+                            Comments:
+                        </td>
+                        <td>
+                            <?php 
+                                echo $_POST["comments"]; 
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <!--Removed submit and reset from two td's here-->
+                    </tr>
+                </table>
                 
             </div>
             
